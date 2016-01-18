8d13ef0a3249a01812da7034be9026700d3bb256
Zeffe <zeffer5789@gmail.com>
2016-01-17 22:25:18 -0600

    Initial Commit

---
+++ SimplisticDK/Form1.Designer.cs
@@ -0,0 +1,143 @@
+﻿namespace SimplisticDK
+{
+    partial class formStart
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(formStart));
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnExit = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pnlTop = new System.Windows.Forms.Panel();
+            this.btnNewBot = new System.Windows.Forms.Button();
+            this.btnNewTrainer = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.btnExit)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(2, 7);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(97, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "SimplisticDK";
+            // 
+            // btnExit
+            // 
+            this.btnExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(23)))), ((int)(((byte)(23)))), ((int)(((byte)(23)))));
+            this.btnExit.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btnExit.BackgroundImage")));
+            this.btnExit.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.btnExit.Location = new System.Drawing.Point(264, 2);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(28, 28);
+            this.btnExit.TabIndex = 1;
+            this.btnExit.TabStop = false;
+            this.btnExit.Click += new System.EventHandler(this.btnExit_Click);
+            // 
+            // label2
+            // 
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.label2.Location = new System.Drawing.Point(0, 34);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(294, 2);
+            this.label2.TabIndex = 2;
+            // 
+            // pnlTop
+            // 
+            this.pnlTop.Location = new System.Drawing.Point(0, 2);
+            this.pnlTop.Name = "pnlTop";
+            this.pnlTop.Size = new System.Drawing.Size(294, 34);
+            this.pnlTop.TabIndex = 3;
+            this.pnlTop.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pnlTop_MouseDown);
+            this.pnlTop.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pnlTop_MouseMove);
+            this.pnlTop.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pnlTop_MouseUp);
+            // 
+            // btnNewBot
+            // 
+            this.btnNewBot.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.btnNewBot.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNewBot.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewBot.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(150)))), ((int)(((byte)(150)))));
+            this.btnNewBot.Location = new System.Drawing.Point(12, 132);
+            this.btnNewBot.Name = "btnNewBot";
+            this.btnNewBot.Size = new System.Drawing.Size(271, 83);
+            this.btnNewBot.TabIndex = 4;
+            this.btnNewBot.Text = "New Bot";
+            this.btnNewBot.UseVisualStyleBackColor = false;
+            // 
+            // btnNewTrainer
+            // 
+            this.btnNewTrainer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(50)))), ((int)(((byte)(50)))), ((int)(((byte)(50)))));
+            this.btnNewTrainer.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnNewTrainer.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnNewTrainer.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(150)))), ((int)(((byte)(150)))), ((int)(((byte)(150)))));
+            this.btnNewTrainer.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.btnNewTrainer.Location = new System.Drawing.Point(12, 43);
+            this.btnNewTrainer.Name = "btnNewTrainer";
+            this.btnNewTrainer.Size = new System.Drawing.Size(271, 83);
+            this.btnNewTrainer.TabIndex = 5;
+            this.btnNewTrainer.Text = "New Trainer";
+            this.btnNewTrainer.UseVisualStyleBackColor = false;
+            // 
+            // formStart
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(77)))), ((int)(((byte)(77)))), ((int)(((byte)(77)))));
+            this.ClientSize = new System.Drawing.Size(295, 227);
+            this.Controls.Add(this.btnNewTrainer);
+            this.Controls.Add(this.btnNewBot);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pnlTop);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "formStart";
+            this.Text = "SimplisticDK";
+            this.Load += new System.EventHandler(this.formStart_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.btnExit)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.PictureBox btnExit;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel pnlTop;
+        private System.Windows.Forms.Button btnNewBot;
+        private System.Windows.Forms.Button btnNewTrainer;
+    }
+}
+
---
+++ SimplisticDK/Form1.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+using System.Runtime.InteropServices;
+
+namespace SimplisticDK
+{
+    public partial class formStart : Form
+    {
+
+        public formStart()
+        {
+            InitializeComponent();
+        }
+
+        public static void pbHighlight(PictureBox pct)
+        {
+            pct.MouseEnter += new EventHandler(pbEnter);
+            pct.MouseLeave += new EventHandler(pbLeave);
+        }
+
+        public static void pbEnter(object sender, EventArgs e)
+        {
+            PictureBox pb = sender as PictureBox;
+            pb.BackColor = Color.FromArgb(pb.BackColor.R + 30, pb.BackColor.G + 30, pb.BackColor.B + 30);
+        }
+
+        public static void pbLeave(object sender, EventArgs e)
+        {
+            PictureBox pb = sender as PictureBox;
+            pb.BackColor = Color.FromArgb(pb.BackColor.R - 30, pb.BackColor.G - 30, pb.BackColor.B - 30);
+        }
+
+        private void formStart_Load(object sender, EventArgs e)
+        {
+            pbHighlight(btnExit);
+        }
+
+        private void btnExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private bool _dragging = false;
+        private Point _start_point = new Point(0, 0);
+
+        private void pnlTop_MouseDown(object sender, MouseEventArgs e)
+        {
+            _dragging = true;  // _dragging is your variable flag
+            _start_point = new Point(e.X, e.Y);
+        }
+
+        private void pnlTop_MouseUp(object sender, MouseEventArgs e)
+        {
+            _dragging = false;
+        }
+
+        private void pnlTop_MouseMove(object sender, MouseEventArgs e)
+        {
+            if (_dragging)
+            {
+                Point p = PointToScreen(e.Location);
+                Location = new Point(p.X - this._start_point.X, p.Y - this._start_point.Y);
+            }
+        }
+    }
+}
